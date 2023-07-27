c90168668ddf13bbd0dc534cdcbadd499c9df1e1
Singh, Yudhishthir <ysingh0923@gmail.com>
2023-07-27 16:06:07 -0400

    Basic ass entity class, with overuse of C++ features

--- 2DGameEngine/src/ECS/ECS.cpp
+++ 2DGameEngine/src/ECS/ECS.cpp
@@ -1,2 +1,13 @@
+#include <vector>
 #include "ECS.h"
+
+void System::AddEntityToSystem(Entity entity) {
+	entities.push_back(entity);
+}
+
+void System::RemoveEntityFromSystem(Entity entity) {
+	entities.erase(std::remove_if(entities.begin(), entities.end(), [&entity](Entity other) {
+		return entity == other;
+	}), entities.end());
+}
 
--- 2DGameEngine/src/ECS/ECS.h
+++ 2DGameEngine/src/ECS/ECS.h
@@ -1,20 +1,60 @@
 #ifndef ECS_H
 #define ECS_H
+#include <bitset>
+#include <vector>
 
-class Component {
+constexpr unsigned int MAX_COMPONENTS = 32;
+typedef std::bitset<MAX_COMPONENTS> Signature;
 
+struct IComponent {
+protected:
+	static int nextId;
+};
+
+template <typename TComponent>
+class Component: public IComponent {
+	static int GetId() {
+		static auto id = nextId++;
+		return id;
+	}
 };
 
 class Entity {
+private:
+	int id;
+public:
+	Entity(int id) : id(id) {};
+	Entity(const Entity& entity) = default;
+	Entity& operator =(const Entity& other) = default;
+	bool operator ==(const Entity& other) const { return id == other.id; }
+	const int GetId() const { return id; }
 
 };
 
 class System {
+private:
+	Signature componentSignature;
+	std::vector<Entity> entities;
+public:
+	System() = default;
+	~System() = default;
 
+	void AddEntityToSystem(Entity entity);
+	void RemoveEntityFromSystem(Entity entity);
+	const std::vector<Entity>& GetSystemEntities() const { return entities; }
+	const Signature& GetComponentSignature() const { return componentSignature; }
+
+	template <typename TComponent> void RequireComponent();
 };
 
 class Registry {
 
 };
+
+template <typename TComponent>
+void System::RequireComponent() {
+	const auto componentId = Component<TComponent>::GetId();
+	componentSignature.set(componentId);
+}
 
 #endif
