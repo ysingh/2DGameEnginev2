9c21e7a55abf01fe520aa71fb61a647cd069b6fd
Singh, Yudhishthir <ysingh0923@gmail.com>
2023-08-01 22:00:30 -0400

    Add debug mode conditionals

--- 2DGameEngine/src/Game/Game.cpp
+++ 2DGameEngine/src/Game/Game.cpp
@@ -12,6 +12,8 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+#define DEBUG
+
 Game::Game() {
 	isRunning = false;
 	Logger::Log("Game constructor called");
@@ -29,23 +31,27 @@ void Game::Initialize() {
 		Logger::Err(SDL_GetError());
 		return;
 	}
-
-	SDL_DisplayMode displayMode;
-	if (SDL_GetCurrentDisplayMode(0, &displayMode) != 0) {
-		Logger::Err("Error getting SDL display mode");
-		Logger::Err(SDL_GetError());
-	}
 	// Better to not scale the window to the users display.
 	// Instead set window mode to fullscreen and let SDL scale the fixed window to that size
 	// The difference is if we scale the window users with higher width and height displays will see more of the game
 	// because we will be trying to fill all those pixels with game data
 	// 
 	// If we keep window size fixed and let sdl scale it then all resolutions will see the same size
+#ifdef DEBUG
 	windowWidth = WINDOW_WIDTH; 
-	//windowWidth = displayMode.w;
-	windowHeight = WINDOW_HEIGHT; //displayMode.h;
-	//windowHeight = displayMode.h;
+	windowHeight = WINDOW_HEIGHT;
+	// Not using refresh rate at the moment. Instead in debug mode we render at 60 fps fixed. In non debug uncapped
+	refreshRate = 60;
+#else
+	SDL_DisplayMode displayMode;
+	if (SDL_GetCurrentDisplayMode(0, &displayMode) != 0) {
+		Logger::Err("Error getting SDL display mode");
+		Logger::Err(SDL_GetError());
+	}
+	windowWidth = displayMode.w;
+	windowHeight = displayMode.h;
 	refreshRate = displayMode.refresh_rate;
+#endif
 
 	// passing NULL for the first param creates a window without titlebar etc.
 	// https://wiki.libsdl.org/SDL2/SDL_CreateWindow
@@ -68,7 +74,9 @@ void Game::Initialize() {
 	// and scale our window to be the size of the screen (as big as possible while preserving aspect ratio)
 	// Is this the same as setting SDL_CreateWindow flag to SDL_WINDOW_FULLSCREEN
 
-	//SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+#ifndef DEBUG
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+#endif
 
 	// https://wiki.libsdl.org/SDL2/SDL_CreateRenderer
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -198,8 +206,9 @@ void Game::ProcessInput() {
 void Game::Update() {
 	// If we are too fast waste some time - this caps the framerate
 	// Commenting this out results in us running at uncapped FPS
-	//int timeToWait = MILLISECONDS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
-	//if (timeToWait > 0 && timeToWait <= MILLISECONDS_PER_FRAME) {
+#ifdef DEBUG
+	int timeToWait = MILLISECONDS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+	if (timeToWait > 0 && timeToWait <= MILLISECONDS_PER_FRAME) {
 		/*
 		The function SDL_Delay is not incredibly accurate, since the call of SDL_Delay itself takes some time to execute.
 		For example, SDL_Delay will never work at a finer resolution than what the OS's scheduler offers.
@@ -207,8 +216,9 @@ void Game::Update() {
 		Modern Linux distros have a 1ms resolution, but the underlying idea still holds.
 		And load is also a factor, meaning that if a system is heavily loaded, we might sleep for hundreds of milliseconds.
 		*/
-	//	SDL_Delay(timeToWait);
-	//}
+		SDL_Delay(timeToWait);
+	}
+#endif
 
 	// Number of seconds elapsed since the last frame
 	double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.f;
